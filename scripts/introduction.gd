9b6d589ade42d721618dd2927f33b2af27621b92
r.chel.i <rachelqi.rq@gmail.com>
2026-01-17 14:19:57 -0500

    Finish intro & criteria dialogue

--- scripts/introduction.gd
+++ scripts/introduction.gd
@@ -1,7 +1,6 @@
 extends Node2D
 
-@onready var bubble = $"Speech Bubble"
-@onready var tail = $SpeechBubbleTail
+@onready var speech_bubble = $"Speech Bubble" 
 @onready var label = $"Speech Bubble/Label"
 
 # Dialogue
@@ -12,51 +11,51 @@ var dialogues = [
 	"I'm an easy client, I promise! I only have a few requests..."
 ]
 var current_index = 0
-var is_showing = false
 
-# Fade transitions
+# Fade in/out transitions
 func fade_in_node(node, duration = 0.3):
+	if not node:
+		return
 	node.visible = true
 	node.modulate.a = 0.0
 	var tween = create_tween()
 	tween.tween_property(node, "modulate:a", 1.0, duration)
 
 func fade_out_node(node, duration = 0.3):
+	if not node:
+		return
 	var tween = create_tween()
 	tween.tween_property(node, "modulate:a", 0.0, duration)
-	var callback := func(): node.visible = false
-	tween.finished.connect(callback)
+	tween.finished.connect(func(): node.visible = false)
 
+# Dialogue functions
 func show_dialogue(index):
 	label.text = dialogues[index]
-	for node in [bubble, tail, label]:
-		fade_in_node(node)
+	fade_in_node(speech_bubble)
 
-func hide_dialogue():
-	for node in [bubble, tail, label]:
-		fade_out_node(node)
+func hide_dialogue(next_index):
+	var tween = fade_out_node(speech_bubble)
+	var timer = Timer.new()
+	timer.one_shot = true
+	timer.wait_time = 0.35
+	add_child(timer)
+	timer.timeout.connect(func():
+		# Exhausted dialogue: switch to next scene
+		if next_index >= dialogues.size():
+			get_tree().change_scene_to_file("res://scenes/criteria.tscn")
+		else:
+			current_index = next_index
+			show_dialogue(current_index)
+		timer.queue_free()
+	)
+	timer.start()
 
 # Click to advance dialogue
 func _unhandled_input(event):
 	if event is InputEventMouseButton and event.pressed:
-		hide_dialogue()
-		current_index += 1
-		if current_index >= dialogues.size():
-			current_index = 0
-		# Delay to await fade-out
-		var timer = Timer.new()
-		timer.one_shot = true
-		timer.wait_time = 0.4
-		add_child(timer)
-		var callback := func():
-			show_dialogue(current_index)
-			timer.queue_free()
-		timer.timeout.connect(callback)
-		timer.start()
+		hide_dialogue(current_index + 1)
 
 func _ready():
-	# Hide everything initially
-	for node in [bubble, tail, label]:
-		node.visible = false
-	# Show dialogue
+	if speech_bubble:
+		speech_bubble.visible = false
 	show_dialogue(current_index)
